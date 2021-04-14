cc392faf4c20d262accd4f5b7f996c41bd9f0f96
[SK C&C] 이상민 <sm.lee1@sk.com>
2021-04-14 18:34:35 +0900

    sidebar added

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+      <nav class="mb-1">
+        <b-nav vertical>
+          <b-nav-item active @click="hide">Active</b-nav-item>
+          <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
+          <b-nav-item href="#link-2" @click="hide">Another Link</b-nav-item>
+        </b-nav>
+      </nav>
+    </b-sidebar>
+  </div>
+</template>
