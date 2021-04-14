03935b40e2b6f9e11f3c02803f9ed68fcd73d79a
[SK C&C] 이상민 <sm.lee1@sk.com>
2021-04-14 15:53:21 +0900

    package init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# temp
+# npm 사용법
 
 ## Project setup
 ```
@@ -22,3 +22,43 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+***
+
+# 개발환경 구성
+#### 1. Vue 프레임워크 설치
+```bash
+$ npm install vue
+```
+#### 2. 어플리케이션 생성 : 상위 폴더에서 create 실행 (idps-web 폴더 생성)
+```bash
+$ vue create idps-web
+```
+- Vue 2.0 preset 선택
+```
+Vue CLI v4.5.12
+? Please pick a preset:
+> Default ([Vue 2] babel, eslint)
+  Default (Vue 3 Preview) ([Vue 3] babel, eslint)
+  Manually select features
+```
+#### 3. 어플리케이션 기본 라이브러리 설치 (idps-web 폴더로 이동)
+```bash
+$ cd idps-web
+$ npm install bootstrap
+$ npm install bootstrap-vue
+$ npm install @popperjs/core
+$ npm install portal-vue
+```
+#### 4. VS Code 사용시 Vetur 추가 설정
+- Vetur(Vue tooling for VS Code)는 syntax-highlighting, error checking, 확장 emmet 등을 지원하며 VS extentions에서 설치
+- 필요시 프로젝트 루트에 jsconfig.json 파일 생성하고 다음과 같은 내용 설정
+```
+{
+    "include": [
+      "./src/**/*"
+    ]
+  }
+```
+#### 5. 기타
+ - 홈페이지 title : public/index.html 파일에서 title 태그에 직접 지정
---
+++ src/components/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="component">
+    <b-card style="max-width: 30rem" class="mb-2">
+    <template #header>
+      <h4 class="mb-0">System Login</h4>
+    </template>
+
+    <b-card-body>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group label="Email:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            placeholder="Enter email"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="Password:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.password"
+            type="password"
+            placeholder="Enter password"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Login</b-button>
+        <b-button type="reset" variant="link" @click="onSubmit"
+          >Sign up</b-button
+        >
+      </b-form>
+    </b-card-body>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.password = "";
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#component {
+  margin: 60px;
+}
+</style>
